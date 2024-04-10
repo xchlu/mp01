39034a9f057e8614cdf3750d7bc067d278d08e64
luchang <luchang2@illinois.edu>
2024-04-10 12:58:35 -0500

    some fix

--- app.py
+++ app.py
@@ -1,90 +1,99 @@
-from shinywidgets import render_plotly
-from shiny.ui import page_navbar
-from functools import partial
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import MultipleLocator
 from shiny import reactive, render, req
 from shiny.express import input, ui
-import pandas as pd;
-from ipyleaflet import Map, Marker  
+import pandas as pd
+from ipyleaflet import Map, Marker
 from shiny.express import ui
-from shinywidgets import render_widget  
+from shinywidgets import render_widget
 from prophet import Prophet
 import process_data
 
 # Load data
 cities = pd.read_csv("data/cities.csv")
 
+
+@reactive.calc
+def get_coordinate_and_lat_lng():
+    return process_data.get_weather_data(latitude=cities.at[int(input.city()), "lat"],
+                                         longitude=cities.at[int(input.city()), "lng"],
+                                         start_date=input.daterange()[0],
+                                         end_date=input.daterange()[1],
+                                         temperature_unit="fahrenheit" if input.units() == "1" else "celsius")
+
+
 # Add page title and sidebar
 ui.page_opts(title="Daily Heat Pump Efficiency Counter", fillable=True)
-with ui.sidebar(bg="#f8f8f8", width=400):  
+with ui.sidebar(bg="#f8f8f8", width=400):
     # set the selected value to be Urbana, Illiois
-    
+
     condition = cities['city_state'] == "Urbana, Illinois"
     urbana = cities[condition].index[0].item()
-    
+
     ui.input_selectize("city", "City", choices=cities["city_state"], selected=urbana, width="100%")
-    
-    @render.text(inline=True)  
+
+
+    @render.text(inline=True)
     def text():
-        # return the lat, lng of the selected city
-        city = input.city()
-        # lat_lng = ', '.join(map(str, cities.iloc[int(city)][["lat", "lng"]].values))
-        # transform the lat, lng to a String without the brackets
-        global coordinate
-        global lat_lng
-        coordinate, lat_lng  = process_data.get_weather_data(latitude=cities.at[int(city),"lat"], longitude=cities.at[int(city),"lng"], start_date=input.daterange()[0], end_date=input.daterange()[1], temperature_unit="fahrenheit" if input.units() == "1" else "celsius")
+        coordinate, lat_lng = get_coordinate_and_lat_lng()
         lat_lng = lat_lng[12:]
         return lat_lng
-    ui.input_date_range("daterange", "Dates", min="2020-01-01" , max="2024-01-01", start="2022-01-01", end="2024-01-01") 
-    ui.input_numeric("numeric", "Years to Forecast", 1, min=1, max=5)  
-    ui.input_radio_buttons(  
-        "trend",  
-        "Forecast Trend",  
-        {"flat": "Flat", "linear": "Linear"},selected="flat"
-    )  
 
-    ui.input_radio_buttons(  
-            "units",  
-            "Units",  
-            {"1": "Fahrenheit", "2": "Celsius"},
-            selected="1"
-    )  
+
+    ui.input_date_range("daterange", "Dates", min="2020-01-01", max="2024-01-01", start="2022-01-01", end="2024-01-01")
+    ui.input_numeric("numeric", "Years to Forecast", 1, min=1, max=5)
+    ui.input_radio_buttons(
+        "trend",
+        "Forecast Trend",
+        {"flat": "Flat", "linear": "Linear"}, selected="flat"
+    )
+
+    ui.input_radio_buttons(
+        "units",
+        "Units",
+        {"1": "Fahrenheit", "2": "Celsius"},
+        selected="1"
+    )
     ui.input_checkbox_group("time", "Plot Options", ["Weekly Rolling Average", "Monthly Rolling Average"], inline=True)
 
-    
+
     @render.ui
     def result():
         x = input.units()
-        
+
         if "1" == x:
-            return ui.input_slider("plot_temp", "Plot Temperature", min=-15, max=50,value=5), ui.input_slider("slider", "Table Temperatures", min=-25, max=60,value=[0, 15])
+            return ui.input_slider("plot_temp", "Plot Temperature", min=-15, max=50, value=5), ui.input_slider("slider",
+                                                                                                               "Table "
+                                                                                                               "Temperatures",
+                                                                                                               min=-25,
+                                                                                                               max=60,
+                                                                                                               value=[0,
+                                                                                                                      15])
         if "2" == x:
-            return ui.input_slider("plot_temp", "Plot Temperature", min=-25, max=10,value=-15), ui.input_slider("slider", "Table Temperatures", min=-30, max=15,value=[-20, -10])  
-    
-    @render_widget  
+            return ui.input_slider("plot_temp", "Plot Temperature", min=-25, max=10, value=-15), ui.input_slider(
+                "slider", "Table Temperatures", min=-30, max=15, value=[-20, -10])
+
+
+    @render_widget
     def map():
+        coordinate, lat_lng = get_coordinate_and_lat_lng()
+
         latlng = lat_lng.split(" ")
         lat = str(latlng[0][:-2])
         lng = str(latlng[1][:-2])
         # print(lat, lng)
-        point = Marker(location=(lat, lng), draggable=False)  
+        point = Marker(location=(lat, lng), draggable=False)
         map = Map(center=(lat, lng), zoom=12)
         map.add_layer(point)
         return map
 
-
-
-  
-with ui.navset_pill(id="tab"): 
+with ui.navset_pill(id="tab"):
     with ui.nav_panel("Historical"):
-        @render.plot(alt="A scatterplot of the lowest temperature over time")  
-        def plot():  
-            
-            coordinate,_ = process_data.get_weather_data(latitude=cities.at[int(input.city()),"lat"], longitude=cities.at[int(input.city()),"lng"], start_date=input.daterange()[0], end_date=input.daterange()[1], temperature_unit="fahrenheit" if input.units() == "1" else "celsius")
-            
-            df = coordinate
+        @render.plot(alt="A scatterplot of the lowest temperature over time")
+        def plot():
+
+            df, _ = get_coordinate_and_lat_lng()
             df['date'] = pd.to_datetime(df['date'])
             fig, ax = plt.subplots()
             threshold = input.plot_temp()
@@ -98,54 +107,48 @@ with ui.navset_pill(id="tab"):
             # set those points which are below the temperature to be transparent
             colors = ['grey' if temp < threshold else 'black' for temp in df['temperature_2m']]
             alpha = [0.5 if temp < threshold else 1 for temp in df['temperature_2m']]
-            ax.scatter(df['date'], df['temperature_2m'], c=colors, alpha=alpha, s=15)            
-            plt.axhline(y=threshold, color='grey', linestyle='-',alpha=0.5)
+            ax.scatter(df['date'], df['temperature_2m'], c=colors, alpha=alpha, s=15)
+            plt.axhline(y=threshold, color='grey', linestyle='-', alpha=0.5)
             y_label = "Daily Minimum Temperature °F" if input.units() == "1" else "Daily Minimum Temperature °C"
             ax.set_ylabel(y_label)
-            ax.yaxis.set_major_locator(MultipleLocator(base=25)) 
+            ax.yaxis.set_major_locator(MultipleLocator(base=25))
             plt.grid()
 
+            return fig
 
-            return fig  
-        
-        
-        
-        # (copilot assisted, but not all accepted) for coordinates data, generate a form including a table which have Temperature and Days below the temperature and proportion of days below the temperature
-        
+
         @render.data_frame
+        @reactive.calc
         def temperature_table():
             # Calculate the temperature and days below the temperature
-            # check out the column names of the dataframe
-            
+            coordinate, _ = get_coordinate_and_lat_lng()
             min = input.slider()[0]
             max = input.slider()[1]
-            temp_range = range(max, min-1, -1)
+            temp_range = range(max, min - 1, -1)
             results = []
             for temp in temp_range:
                 below = coordinate["temperature_2m"] < temp
                 proportion_of_below = below.mean().round(3)
-                
-                results.append({'Temp': temp,'Days Below':below.sum(),
-                    'Proportion Below': proportion_of_below})
+
+                results.append({'Temp': temp, 'Days Below': below.sum(),
+                                'Proportion Below': proportion_of_below})
             df = pd.DataFrame(results)
-            return render.DataGrid(df, row_selection_mode='multiple', width= '100%', height='auto', summary='')
-        
+            return render.DataGrid(df, row_selection_mode='multiple', width='100%', height='auto', summary='')
+
     with ui.nav_panel("Forcast"):
-        @render.plot(alt="A forcast scatterplot of the lowest temperature over time")  
+        @render.plot(alt="A forcast scatterplot of the lowest temperature over time")
         def forecast():
-            # get the forecast data
-            coordinate,_ = process_data.get_weather_data(latitude=cities.at[int(input.city()),"lat"], longitude=cities.at[int(input.city()),"lng"], start_date=input.daterange()[0], end_date=input.daterange()[1], temperature_unit="fahrenheit" if input.units() == "1" else "celsius")
+            coordinate, _ = get_coordinate_and_lat_lng()
             df = coordinate
             df['date'] = pd.to_datetime(df['date']).dt.tz_localize(None)
-            
-            
-            model = Prophet(growth=input.trend(), interval_width= 0.95)
+
+            model = Prophet(growth=input.trend(), interval_width=0.95)
             df_prophet = df.rename(columns={"date": "ds", "temperature_2m": "y"})
             model.fit(df_prophet)
-            future = model.make_future_dataframe(periods=365*int(input.numeric()))
+            future = model.make_future_dataframe(periods=365 * int(input.numeric()))
             global forecast
             forecast = model.predict(future)
-            
+
             last_date = df_prophet['ds'].max()
 
             forecast_future = forecast[forecast['ds'] > last_date]
@@ -154,31 +157,29 @@ with ui.navset_pill(id="tab"):
             fig.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 
             # set those points which are below the temperature to be transparent
-            plt.axhline(y=threshold, color='grey', linestyle='-',alpha=0.5)
+            plt.axhline(y=threshold, color='grey', linestyle='-', alpha=0.5)
             y_label = "Daily Minimum Temperature °F" if input.units() == "1" else "Daily Minimum Temperature °C"
-            # set the y label
             fig.gca().set_ylabel(y_label)
 
             return fig
+
+
         @render.data_frame
         def forcast_temperature_table():
-            # reactive when the forecast is generated
-            trend = input.trend()
+            get_coordinate_and_lat_lng()
             df = forecast
             min = input.slider()[0]
             max = input.slider()[1]
-            temp_range = range(max, min-1, -1)
+            temp_range = range(max, min - 1, -1)
             results = []
             for temp in temp_range:
-                # get the temperature and days below the temperature
                 below = df["yhat_lower"] < temp
                 proportion_of_below = below.mean().round(3)
-                
-                results.append({'Temp': temp,'Days Below':below.sum(),
-                    'Proportion Below': proportion_of_below})
-            df = pd.DataFrame(results)
-            return render.DataGrid(df, row_selection_mode='multiple', width= '100%', height='auto', summary='')
 
+                results.append({'Temp': temp, 'Days Below': below.sum(),
+                                'Proportion Below': proportion_of_below})
+            df = pd.DataFrame(results)
+            return render.DataGrid(df, row_selection_mode='multiple', width='100%', height='auto', summary='')
 
     with ui.nav_panel("About"):
         ui.markdown(
@@ -191,7 +192,7 @@ with ui.navset_pill(id="tab"):
             performance specifications and learn if the heat pump works well in extremely
             cold environments.
 
-            This applicaiton provides users with historical daily minimum temperature data (a plot and a table) from 
+            This application provides users with historical daily minimum temperature data (a plot and a table) from 
             cities in the whole United States. Additionally, you are able to access forecast data
             up to **five years**. Hope this will help you decide which heat pump suits you best,
             or maybe get a furnace.
@@ -215,5 +216,4 @@ with ui.navset_pill(id="tab"):
             Weather data source: Our weather data is from [Open-Meteo](https://open-meteo.com/) 
             and their[Historical Weather API](https://open-meteo.com/en/docs/historical-weather-api).
             """
-)
-
+        )
--- process_data.py
+++ process_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from retry_requests import retry
 
 
-def get_weather_data(**kwargs ):
+def get_weather_data(**kwargs):
     # parse the parameters
     latitude = kwargs.get("latitude")
     longitude = kwargs.get("longitude")
@@ -20,47 +20,42 @@ def get_weather_data(**kwargs ):
     url = "https://archive-api.open-meteo.com/v1/archive"
 
     params = {
-        "latitude": latitude,               # input
-        "longitude": longitude,              # input
-        "start_date": start_date,      # input
-        "end_date": end_date,        # input
-        "daily": "temperature_2m_min",   # fixed
-        "temperature_unit": temperature_unit # input
+        "latitude": latitude,  # input
+        "longitude": longitude,  # input
+        "start_date": start_date,  # input
+        "end_date": end_date,  # input
+        "daily": "temperature_2m_min",  # fixed
+        "temperature_unit": temperature_unit  # input
     }
 
     # Get the data from the Open-Meteo API
     responses = openmeteo.weather_api(url, params=params)
     response = responses[0]
-    
+
     daily = response.Daily()
     daily_temperature_2m = daily.Variables(0).ValuesAsNumpy()
-    
+
     # this part is copied from the Open-Meteo API documentation
     daily_data = {"date": pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-        freq = pd.Timedelta(seconds = daily.Interval()),
-        inclusive = "left"
-    )}
-    daily_data["temperature_2m"] = daily_temperature_2m
-    daily_dataframe = pd.DataFrame(data = daily_data)
-    
+        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+        end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+        freq=pd.Timedelta(seconds=daily.Interval()),
+        inclusive="left"
+    ), "temperature_2m": daily_temperature_2m}
+    daily_dataframe = pd.DataFrame(data=daily_data)
+
     return daily_dataframe, f"Coordinates {response.Latitude()}°N {response.Longitude()}°E"
 
 
 def process_cities():
-    # read the csv file in data-raw
     cities = pd.read_csv("data-raw/uscities.csv")
     print(cities.head(5))
     print(cities.columns)
-    # filter the cities to only have the population greater than 10000
     cities = cities[cities["population"] > 10000]
-    # merge the 'city' and 'state_name' columns to create a new column 'city_state'
+    # merge
     cities["city_state"] = cities["city"] + ", " + cities["state_name"]
     print(cities.head(5))
-    # filter the columns to only have 'city_state', 'lat', and 'lng'
+    # filter
     cities = cities[["city_state", "lat", "lng"]]
-    # check the total rows of the dataframe
     print(cities.shape)
-    # save the file to cities.csv under /data directory
     cities.to_csv("data/cities.csv", index=False)
